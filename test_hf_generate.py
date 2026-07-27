a6f483761aa9a253b570bc4f8e70b38d514f9031
Jaip Kapoor <jaipkapoor99@gmail.com>
2026-07-27 17:08:33 +0000

    update test_hf_generate.py with top-p, top-k nucleus sampling

--- test_hf_generate.py
+++ test_hf_generate.py
@@ -37,6 +37,8 @@ def main():
             max_new_tokens=args.max_tokens,
             do_sample=True,
             temperature=args.temperature,
+            top_k=50,
+            top_p=0.9,
             repetition_penalty=1.15,
             pad_token_id=tokenizer.eos_token_id
         )
